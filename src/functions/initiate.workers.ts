dc2aa5422dd909aa9bdf84acbad018248dda16ef
cope <predrag@stojadinovic.net>
2021-11-23 15:15:33 +0100

    optimizing cronjobs/workers startup

--- src/functions/initiate.workers.ts
+++ src/functions/initiate.workers.ts
@@ -7,16 +7,19 @@ import {filter, isFunction} from 'lodash';
 
 import WorkerType from '../_types/worker.type';
 
+const _asyncedStart = (worker: WorkerType): void => {
+	const {init, work} = worker;
+	if (isFunction(init)) init().then(() => work?.());
+	else work?.();
+};
+
 const initiateWorkers = (folder: string): void => {
 	const fileNames = fs.readdirSync(folder);
 	const files = filter(fileNames, (name) => !fs.lstatSync(path.join(folder, name)).isDirectory());
 	files.forEach((file: string) => {
 		const absoluteFilePath = path.join(folder, file);
 		const worker: WorkerType = require(absoluteFilePath).default;
-		const {init, work} = worker;
-
-		if (isFunction(init)) init().then(() => work?.());
-		else work?.();
+		setTimeout(() => _asyncedStart(worker));
 	});
 
 	const folders = filter(fileNames, (name: string) => fs.lstatSync(path.join(folder, name)).isDirectory());
