26fa1aad6dd27f9dea929d96ea72a09fd05f8907
cope <predrag@stojadinovic.net>
2021-11-23 15:45:35 +0100

    reverting cronjobs/workers startup optimization

--- src/functions/initiate.cronjobs.ts
+++ src/functions/initiate.cronjobs.ts
@@ -8,19 +8,16 @@ import * as cron from 'node-cron';
 
 import CronJobType from '../_types/cronjob.type';
 
-const _asyncedStart = (cronjob: CronJobType): void => {
-	const {schedule, job, options, init} = cronjob;
-	if (isFunction(init)) init().then(() => cron.schedule(schedule, job, options));
-	else cron.schedule(schedule, job, options);
-};
-
 const initiateCronjobs = (folder: string): void => {
 	const fileNames = fs.readdirSync(folder);
 	const files = filter(fileNames, (name) => !fs.lstatSync(path.join(folder, name)).isDirectory());
 	files.forEach((file: string) => {
 		const absoluteFilePath = path.join(folder, file);
 		const cronjob: CronJobType = require(absoluteFilePath).default;
-		setTimeout(() => _asyncedStart(cronjob));
+
+		const {schedule, job, options, init} = cronjob;
+		if (isFunction(init)) init().then(() => cron.schedule(schedule, job, options));
+		else cron.schedule(schedule, job, options);
 	});
 
 	const folders = filter(fileNames, (name: string) => fs.lstatSync(path.join(folder, name)).isDirectory());
