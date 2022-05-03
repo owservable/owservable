8eb087d9fe0929248a4a65422462c027e9acbf37
cope <predrag@stojadinovic.net>
2022-05-03 12:05:12 +0200

    docs

--- src/store/factories/store.factory.ts
+++ src/store/factories/store.factory.ts
@@ -4,13 +4,14 @@ import {Model} from 'mongoose';
 
 import AStore from '../a.store';
 import CountStore from '../count.store';
-import CollectionsModelsMap from '../../mongodb/collections.models.map';
-import CollectionStore from '../collection.store';
 import DocumentStore from '../document.store';
+import CollectionStore from '../collection.store';
 import StoreScopeType from '../../_types/store.scope.type';
+import CollectionsModelsMap from '../../mongodb/collections.models.map';
 
 const storeFactory = (scope: StoreScopeType, observe: string, target: string): AStore => {
 	const model: Model<any> = CollectionsModelsMap.getModelByCollection(observe);
+
 	if (scope === 'many') return new CollectionStore(model, target);
 	if (scope === 'count') return new CountStore(model, target);
 	return new DocumentStore(model, target);
