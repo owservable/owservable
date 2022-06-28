66dfb364cbf0638f0b56d42aee0790da4549c288
cope <predrag@stojadinovic.net>
2022-06-28 13:19:35 +0200

    reverting back to mongo 3.6

--- src/mongodb/functions/observable.model.ts
+++ src/mongodb/functions/observable.model.ts
@@ -1,32 +1,44 @@
 'use strict';
 
 import {pick} from 'lodash';
-import {Subject, Subscription} from 'rxjs';
+import {Subject} from 'rxjs';
 import {Model} from 'mongoose';
-import {filter} from 'rxjs/operators';
-import observableDatabase from './observable.database';
+import {ChangeStream} from 'mongodb';
 
 class ObservableModel extends Subject<any> {
-	private readonly _collection: string;
-	protected _subscription: Subscription;
+	private _model: Model<any>;
+	private _stream: ChangeStream;
 
-	constructor(collection: string) {
+	constructor(model: Model<any>) {
 		super();
-		this._collection = collection;
+		this._model = model;
+		this._stream = this._model.watch([], {fullDocument: 'updateLookup'});
 
-		this._subscription = observableDatabase()
-			.pipe(filter((change) => this._pipeFilter(change)))
-			.subscribe({
-				next: (change: any): void => this.next(pick(change, ['ns', 'documentKey', 'operationType', 'updateDescription', 'fullDocument']))
-			});
+		this._stream.on('change', (change) => {
+			this.next(pick(change, ['ns', 'documentKey', 'operationType', 'updateDescription', 'fullDocument']));
+		});
 	}
 
-	private _pipeFilter(change: any): boolean {
-		const {
-			ns: {coll}
-		} = change;
-		return this._collection === coll;
-	}
+	// TODO: Available from MongoDB 4.0
+	// private readonly _collection: string;
+	// protected _subscription: Subscription;
+	//
+	// constructor(collection: string) {
+	// 	super();
+	// 	this._collection = collection;
+	//
+	// 	this._subscription = observableDatabase()
+	// 		.pipe(filter((change) => this._pipeFilter(change)))
+	// 		.subscribe({
+	// 			next: (change: any): void => this.next(pick(change, ['ns', 'documentKey', 'operationType', 'updateDescription', 'fullDocument']))
+	// 		});
+	// }
+	// private _pipeFilter(change: any): boolean {
+	// 	const {
+	// 		ns: {coll}
+	// 	} = change;
+	// 	return this._collection === coll;
+	// }
 }
 
 class ObservableModelsMap {
@@ -41,7 +53,9 @@ class ObservableModelsMap {
 		const instance = ObservableModelsMap.init();
 		const map = instance._map;
 		const collectionName = model.collection.collectionName;
-		if (!map.get(collectionName)) map.set(collectionName, new ObservableModel(collectionName));
+		if (!map.get(collectionName)) map.set(collectionName, new ObservableModel(model));
+		// TODO: Available from MongoDB 4.0
+		// if (!map.get(collectionName)) map.set(collectionName, new ObservableModel(collectionName));
 		return map.get(collectionName);
 	}
 
