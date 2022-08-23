a1b198d0e57629b202d1a6d12bad69ca0d7cede9
cope <predrag@stojadinovic.net>
2022-08-23 15:52:28 +0200

    chore: changing mongo connector to async/await

--- src/mongodb/mongodb.connector.ts
+++ src/mongodb/mongodb.connector.ts
@@ -4,31 +4,35 @@ import * as mongoose from 'mongoose';
 import {Connection} from 'mongoose';
 
 export default class MongoDBConnector {
-	public static init(mongoDbUri: string): Connection {
-		if (!MongoDBConnector._instance) MongoDBConnector._instance = new MongoDBConnector(mongoDbUri);
-		return MongoDBConnector._instance._connection;
+	public static async init(mongoDbUri: string): Promise<Connection> {
+		if (!this._connection) {
+			mongoose
+				.connect(mongoDbUri, {
+					poolSize: 10,
+					// useCreateIndex: true,	// does not work in MongoDB 5
+					useNewUrlParser: true,
+					useUnifiedTopology: true
+				})
+				.then(() => {
+					this._connection = mongoose.connection;
+					this._connection.on('error', console.error.bind(console, 'connection error:'));
+					this._connection.once('open', () => console.log('ows -> MongoDB connected to', mongoDbUri));
+
+					return this._connection;
+				})
+				.catch(console.error);
+
+			//
+		} else {
+			return this._connection;
+		}
 	}
 
 	public static get connection(): Connection {
-		return MongoDBConnector._instance._connection;
+		return this._connection;
 	}
 
-	private static _instance: MongoDBConnector;
-	private readonly _connection: Connection;
+	private static _connection: Connection;
 
-	private constructor(mongoDbUri: string) {
-		mongoose
-			.connect(mongoDbUri, {
-				poolSize: 10,
-				// useCreateIndex: true,	// does not work in MongoDB 5
-				useNewUrlParser: true,
-				useUnifiedTopology: true
-			})
-			.then(() => ({}))
-			.catch(console.error);
-
-		this._connection = mongoose.connection;
-		this._connection.on('error', console.error.bind(console, 'connection error:'));
-		this._connection.once('open', () => console.log('ows -> MongoDB connected to', mongoDbUri));
-	}
+	private constructor() {}
 }
